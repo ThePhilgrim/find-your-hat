5c0c9128c2480bea4b83c4af4a5a6bc99d173070
ThePhilgrim <76887896+ThePhilgrim@users.noreply.github.com>
2021-11-11 19:30:28 +0100

    Create logic to get current board state

--- main.js
+++ main.js
@@ -5,7 +5,38 @@ const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
 
+class Field {
+  constructor(field) {
+    this.field = field;
+    this.playerXPosition = 0;
+    this.playerYPosition = 0;
+  }
 
+  getCurrentBoardState() {
+    for (let i = 0; i < this.field.length; i++) {
+      console.log(this.field[i].join(''));
+    }
+  }
+
+  changePlayerPosition(event) {
+
+  }
+
+  checkIfGameLost() {
+
+  }
+}
+
+
+
+
+const myField = new Field([
+  ['*', '░', 'O'],
+  ['░', 'O', '░'],
+  ['░', '^', '░'],
+]);
+
+myField.getCurrentBoardState();
 
 
 /*
